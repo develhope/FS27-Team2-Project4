4469cbd4e27f7ad24d59a72e60a2b76fc99d83e3
Andros924 <sir_vivi@hotmail.it>
2024-07-27 15:29:14 +0200

    aggiunti bottoni accedi e registrati sulla navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -48,30 +48,43 @@ function Navbar() {
                 Shop
               </Link>
             </li>
+            <li>
+              <Link to="/blog" className="hover:text-[#f6bcba] transition duration-300">
+                Blog
+              </Link>
+            </li>
           </ul>
         </div>
 
-        <div className="relative">
-          <button 
-            onClick={toggleDropdown} 
-            className="hover:text-[#f6bcba] transition duration-300"
-          >
-            Contatti
-          </button>
-          <ul 
-            className={`absolute top-full right-0 mt-2 bg-white shadow-lg rounded-md p-2 ${dropdownOpen ? "block" : "hidden"}`}
-          >
-            <li>
-              <a href="https://www.facebook.com/" className="block px-4 py-2 hover:bg-gray-100">
-                Facebook
-              </a>
-            </li>
-            <li>
-              <a href="https://www.instagram.com/" className="block px-4 py-2 hover:bg-gray-100">
-                Instagram
-              </a>
-            </li>
-          </ul>
+        <div className="flex items-center space-x-4">
+          <div className="relative">
+            <button 
+              onClick={toggleDropdown} 
+              className="hover:text-[#f6bcba] transition duration-300"
+            >
+              Contatti
+            </button>
+            <ul 
+              className={`absolute top-full right-0 mt-2 bg-white shadow-lg rounded-md p-2 ${dropdownOpen ? "block" : "hidden"}`}
+            >
+              <li>
+                <a href="https://www.facebook.com/" className="block px-4 py-2 hover:bg-gray-100">
+                  Facebook
+                </a>
+              </li>
+              <li>
+                <a href="https://www.instagram.com/" className="block px-4 py-2 hover:bg-gray-100">
+                  Instagram
+                </a>
+              </li>
+            </ul>
+          </div>
+          <Link to="/login" className="bg-black text-white px-4 py-2 rounded hover:bg-gray-800 transition duration-300">
+            Accedi
+          </Link>
+          <Link to="/register" className="bg-white text-black px-4 py-2 rounded border border-black hover:bg-gray-100 transition duration-300">
+            Registrati
+          </Link>
         </div>
       </div>
     </nav>
