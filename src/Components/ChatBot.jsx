c5fb021bd22b67b1c87b7c68408187964016decc
Manuel Cau <caumanuel10@gmail.com>
2024-07-31 10:18:13 +0200

    images

--- src/Components/ChatBot.jsx
+++ src/Components/ChatBot.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import { FaComments } from "react-icons/fa";
 
 const teamMembers = [
