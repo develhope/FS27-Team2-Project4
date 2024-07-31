93165068cb16a1db8e5506dba2c8e3362bd16e02
Andros924 <sir_vivi@hotmail.it>
2024-07-31 10:09:21 +0200

    foto banner risoluzione un pò migliore

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -7,7 +7,7 @@ return (
   
 
 <section
-  className="relative bg-[url(../src/assets/images/cld-header.jpg)] bg-cover bg-center bg-no-repeat"
+  className="relative bg-[url(../src/assets/images/cld-header.jpeg)] bg-cover bg-center bg-no-repeat"
 >
   <div
     className="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
