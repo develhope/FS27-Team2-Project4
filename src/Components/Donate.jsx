efa7f8684d72f26ea38ee61fa00afbe6b730b474
Andros924 <sir_vivi@hotmail.it>
2024-08-18 10:57:04 +0200

    alcune migliorie

--- src/Components/Donate.jsx
+++ src/Components/Donate.jsx
@@ -17,7 +17,7 @@ function Donate() {
     paypalDispatch({
       type: "resetOptions",
       value: {
-        "client-id": "YOUR_PAYPAL_CLIENT_ID", //mettere qui il codice id di paypal
+        "client-id": "YOUR_PAYPAL_CLIENT_ID",
         currency: "EUR",
       },
     });
@@ -25,22 +25,19 @@ function Donate() {
 
   const handleDonationSubmit = async (event) => {
     event.preventDefault();
-
     if (!donationAmount || donationAmount <= 0) {
       setDonationError(
         "L'importo della donazione deve essere maggiore di zero."
       );
       return;
     }
-
     setDonationError(null);
-
     if (!donationMethod) {
       setDonationError("Per favore, scegli un metodo di donazione.");
       return;
     }
-
     if (donationMethod === "paypal") {
+      // Handle PayPal donation
     } else if (donationMethod === "iban") {
       setShowIbanDetails(true);
     } else if (donationMethod === "5x1000") {
@@ -49,29 +46,31 @@ function Donate() {
   };
 
   return (
-    <div className="donate-page font-sans text-gray-800 mt-20">
-      <div className="flex flex-col md:flex-row">
-        <div className="md:w-1/2">
+    <div className="donate-page font-sans text-gray-800 mt-8">
+      <div className="flex flex-col lg:flex-row">
+        <div className="lg:w-1/2">
           <img
             src={bannerImage}
             alt="Banner donazioni"
-            className="w-full h-[600px] object-cover"
+            className="w-full h-64 lg:h-full object-cover"
           />
         </div>
 
-        <div className="md:w-1/2 md:ml-8 mt-8 md:mt-0 p-8 bg-white rounded-lg shadow-md">
-          <form onSubmit={handleDonationSubmit}>
-            <h1 className="text-2xl font-semibold text-center mb-4 text-[#526742]">
+        <div className="lg:w-1/2 p-4 lg:p-8 bg-white rounded-lg shadow-md">
+          <form onSubmit={handleDonationSubmit} className="space-y-6">
+            <h1 className="text-2xl font-semibold text-center text-[#526742]">
               Fai la differenza, invia il tuo aiuto
             </h1>
-            <h2 className="text-2xl font-semibold text-center mb-6 text-[#92aa7f]">
+            <h2 className="text-xl font-semibold text-center text-[#92aa7f]">
               Scegli l'importo
             </h2>
-            <div className="grid grid-cols-2 gap-4 mb-4">
+            <div className="grid grid-cols-2 gap-4">
               <button
                 type="button"
-                className={`border rounded-lg py-2 text-center focus:outline-none ${
-                  donationAmount === "15" ? "bg-[#92aa7f] text-white" : ""
+                className={`border rounded-lg py-2 text-center focus:outline-none transition duration-300 ${
+                  donationAmount === "15"
+                    ? "bg-[#92aa7f] text-white"
+                    : "hover:bg-gray-100"
                 }`}
                 onClick={() => setDonationAmount("15")}
               >
@@ -79,8 +78,10 @@ function Donate() {
               </button>
               <button
                 type="button"
-                className={`border rounded-lg py-2 text-center focus:outline-none ${
-                  donationAmount === "25" ? "bg-[#92aa7f] text-white" : ""
+                className={`border rounded-lg py-2 text-center focus:outline-none transition duration-300 ${
+                  donationAmount === "25"
+                    ? "bg-[#92aa7f] text-white"
+                    : "hover:bg-gray-100"
                 }`}
                 onClick={() => setDonationAmount("25")}
               >
@@ -88,8 +89,10 @@ function Donate() {
               </button>
               <button
                 type="button"
-                className={`border rounded-lg py-2 text-center focus:outline-none ${
-                  donationAmount === "50" ? "bg-[#92aa7f] text-white" : ""
+                className={`border rounded-lg py-2 text-center focus:outline-none transition duration-300 ${
+                  donationAmount === "50"
+                    ? "bg-[#92aa7f] text-white"
+                    : "hover:bg-gray-100"
                 }`}
                 onClick={() => setDonationAmount("50")}
               >
@@ -100,17 +103,17 @@ function Donate() {
                 placeholder="€ Altro importo"
                 value={donationAmount}
                 onChange={(e) => setDonationAmount(e.target.value)}
-                className="border rounded-lg py-2 px-3 focus:outline-none"
+                className="border rounded-lg py-2 px-3 focus:outline-none focus:ring-2 focus:ring-[#92aa7f]"
               />
             </div>
 
-            <h2 className="text-2xl font-semibold text-center mb-4 text-[#1e1e1e]">
+            <h2 className="text-xl font-semibold text-center text-[#1e1e1e]">
               Metodo di pagamento
             </h2>
             <select
               value={donationMethod}
               onChange={(e) => setDonationMethod(e.target.value)}
-              className="w-full border rounded-lg py-2 px-3 focus:outline-none mb-4"
+              className="w-full border rounded-lg py-2 px-3 focus:outline-none focus:ring-2 focus:ring-[#92aa7f]"
             >
               <option value="">Seleziona un metodo</option>
               <option value="paypal">PayPal</option>
@@ -119,8 +122,8 @@ function Donate() {
             </select>
 
             {showIbanDetails && (
-              <div className="mb-4">
-                <h3 className="font-bold">Coordinate Bancarie:</h3>
+              <div className="bg-gray-100 p-4 rounded-lg">
+                <h3 className="font-bold mb-2">Coordinate Bancarie:</h3>
                 <p>IBAN: IT00 XXXX XXXX XXXX XXXX XXXX</p>
                 <p>Intestatario: [Nome dell'organizzazione]</p>
                 <p>Causale: Donazione per [Nome della campagna]</p>
@@ -128,15 +131,15 @@ function Donate() {
             )}
 
             {show5x1000Details && (
-              <div className="mb-4">
-                <h3 className="font-bold">5x1000:</h3>
+              <div className="bg-gray-100 p-4 rounded-lg">
+                <h3 className="font-bold mb-2">5x1000:</h3>
                 <p>Codice Fiscale: [Codice Fiscale dell'organizzazione]</p>
                 <p>Firma nel riquadro "Sostegno del volontariato..."</p>
               </div>
             )}
 
             {donationError && (
-              <p className="text-red-500 font-bold mb-2">{donationError}</p>
+              <p className="text-red-500 font-bold">{donationError}</p>
             )}
 
             <button
@@ -154,98 +157,115 @@ function Donate() {
         </div>
       </div>
 
-      <div className="max-w-md mx-auto mt-8 p-4 bg-gray-100 rounded-lg">
-        <h1 className="text-2xl font-semibold text-center mb-4 text-[#526742]">
+      <div className="max-w-7xl mx-auto mt-8 p-4 bg-gray-100 rounded-lg">
+        <h1 className="text-4xl lg:text-6xl font-semibold text-center mb-8 text-[#526742]">
           Il Potere di una Donazione
         </h1>
-        <h2 className="text-xl font-medium text-center mb-2 text-[#92aa7f]">
-          Trasformare un Piccolo Gesto in un Grande Cambiamento
-        </h2>
-        <p className="text-center text-sm">
-          Quando pensiamo alla parola "donazione", spesso immaginiamo semplici
-          atti di generosità, gesti che, a prima vista, sembrano piccoli. Ma la
-          realtà è che ogni donazione è una scintilla che può innescare un
-          grande cambiamento. Ogni volta che scegli di donare, stai dando vita a
-          un'azione che si propaga, che tocca vite, e che costruisce un ponte
-          verso un futuro migliore.
-        </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
+          <div className="bg-white p-6 rounded-lg border-2 border-[#526742] shadow-md">
+            <h2 className="text-xl font-medium text-center mb-4 text-[#92aa7f]">
+              Trasformare un Piccolo Gesto in un Grande Cambiamento
+            </h2>
+            <p className="text-justify text-sm">
+              Quando pensiamo alla parola "donazione", spesso immaginiamo
+              semplici atti di generosità, gesti che, a prima vista, sembrano
+              piccoli. Ma la realtà è che ogni donazione è una scintilla che può
+              innescare un grande cambiamento. Ogni volta che scegli di donare,
+              stai dando vita a un'azione che si propaga, che tocca vite, e che
+              costruisce un ponte verso un futuro migliore.
+            </p>
+          </div>
 
-        <h2 className="text-xl font-medium text-center mt-6 mb-2 text-[#92aa7f]">
-          Un Futuro Che Dipende da Te
-        </h2>
-        <p className="text-center text-sm">
-          Immagina un mondo in cui ogni bambino, ogni animale, ogni persona
-          vulnerabile, riceva l'aiuto di cui ha bisogno. Questo mondo non è
-          un'utopia, è una realtà che possiamo costruire insieme. La tua
-          donazione è il mattone con cui stiamo edificando un domani più giusto
-          e più sicuro. Non è solo una questione di risorse, è una questione di
-          visione, di credere che un futuro diverso sia possibile e che ognuno
-          di noi abbia il potere di plasmarlo.
-        </p>
+          <div className="bg-white p-6 rounded-lg border-2 border-[#526742] shadow-md">
+            <h2 className="text-xl font-medium text-center mb-4 text-[#92aa7f]">
+              Un Futuro Che Dipende da Te
+            </h2>
+            <p className="text-justify text-sm">
+              Immagina un mondo in cui ogni bambino, ogni animale, ogni persona
+              vulnerabile, riceva l'aiuto di cui ha bisogno. Questo mondo non è
+              un'utopia, è una realtà che possiamo costruire insieme. La tua
+              donazione è il mattone con cui stiamo edificando un domani più
+              giusto e più sicuro. Non è solo una questione di risorse, è una
+              questione di visione, di credere che un futuro diverso sia
+              possibile e che ognuno di noi abbia il potere di plasmarlo.
+            </p>
+          </div>
 
-        <h2 className="text-xl font-medium text-center mt-6 mb-2 text-[#92aa7f]">
-          Il Valore della Solidarietà
-        </h2>
-        <p className="text-center text-sm">
-          Viviamo in un mondo dove l'individualismo spesso prende il
-          sopravvento, ma è nella solidarietà che troviamo la nostra forza più
-          grande. Quando doni, non stai solo aiutando qualcuno in difficoltà,
-          stai dicendo al mondo che credi nel valore della comunità, che nessuno
-          dovrebbe essere lasciato solo ad affrontare le sfide della vita. La
-          tua donazione è un segno tangibile di questa solidarietà, un modo per
-          dire "sono con te" a chi ne ha più bisogno.
-        </p>
+          <div className="bg-white p-6 rounded-lg border-2 border-[#526742] shadow-md">
+            <h2 className="text-xl font-medium text-center mb-4 text-[#92aa7f]">
+              Il Valore della Solidarietà
+            </h2>
+            <p className="text-justify text-sm">
+              Viviamo in un mondo dove l'individualismo spesso prende il
+              sopravvento, ma è nella solidarietà che troviamo la nostra forza
+              più grande. Quando doni, non stai solo aiutando qualcuno in
+              difficoltà, stai dicendo al mondo che credi nel valore della
+              comunità, che nessuno dovrebbe essere lasciato solo ad affrontare
+              le sfide della vita. La tua donazione è un segno tangibile di
+              questa solidarietà, un modo per dire "sono con te" a chi ne ha più
+              bisogno.
+            </p>
+          </div>
 
-        <h2 className="text-xl font-medium text-center mt-6 mb-2 text-[#92aa7f]">
-          Oltre la Donazione: Creare un Impatto Duraturo
-        </h2>
-        <p className="text-center text-sm">
-          Ogni donazione è un seme piantato in un terreno fertile. Ma come ogni
-          seme, ha bisogno di cure costanti per crescere e dare frutto. La tua
-          donazione non si esaurisce nel momento in cui viene fatta, continua a
-          vivere, a crescere, a moltiplicarsi. Permette a progetti di lunga
-          durata di vedere la luce, di educare, di proteggere, di guarire.
-          Attraverso il tuo contributo, stiamo costruendo qualcosa di duraturo,
-          un impatto che si estenderà ben oltre il presente, toccando
-          generazioni future.
-        </p>
+          <div className="bg-white p-6 rounded-lg border-2 border-[#526742] shadow-md">
+            <h2 className="text-xl font-medium text-center mb-4 text-[#92aa7f]">
+              Oltre la Donazione: Creare un Impatto Duraturo
+            </h2>
+            <p className="text-justify text-sm">
+              Ogni donazione è un seme piantato in un terreno fertile. Ma come
+              ogni seme, ha bisogno di cure costanti per crescere e dare frutto.
+              La tua donazione non si esaurisce nel momento in cui viene fatta,
+              continua a vivere, a crescere, a moltiplicarsi. Permette a
+              progetti di lunga durata di vedere la luce, di educare, di
+              proteggere, di guarire. Attraverso il tuo contributo, stiamo
+              costruendo qualcosa di duraturo, un impatto che si estenderà ben
+              oltre il presente, toccando generazioni future.
+            </p>
+          </div>
 
-        <h2 className="text-xl font-medium text-center mt-6 mb-2 text-[#92aa7f]">
-          Un Piccolo Atto di Generosità, Un Grande Cuore
-        </h2>
-        <p className="text-center text-sm">
-          Non importa l'entità della donazione, ciò che conta è il cuore che c'è
-          dietro. Ogni contributo, grande o piccolo, è un segno di speranza. È
-          la dimostrazione che, in un mondo a volte freddo e distante, esiste
-          ancora calore, empatia, e amore per il prossimo. La tua donazione è un
-          riflesso del tuo cuore, un cuore che non si arrende di fronte alle
-          difficoltà, che cerca sempre di fare la differenza, ovunque e
-          comunque.
-        </p>
+          <div className="bg-white p-6 rounded-lg border-2 border-[#526742] shadow-md">
+            <h2 className="text-xl font-medium text-center mb-4 text-[#92aa7f]">
+              Un Piccolo Atto di Generosità, Un Grande Cuore
+            </h2>
+            <p className="text-justify text-sm">
+              Non importa l'entità della donazione, ciò che conta è il cuore che
+              c'è dietro. Ogni contributo, grande o piccolo, è un segno di
+              speranza. È la dimostrazione che, in un mondo a volte freddo e
+              distante, esiste ancora calore, empatia, e amore per il prossimo.
+              La tua donazione è un riflesso del tuo cuore, un cuore che non si
+              arrende di fronte alle difficoltà, che cerca sempre di fare la
+              differenza, ovunque e comunque.
+            </p>
+          </div>
 
-        <h2 className="text-xl font-medium text-center mt-6 mb-2 text-[#92aa7f]">
-          L'Unione fa la Forza
-        </h2>
-        <p className="text-center text-sm">
-          Insieme, possiamo fare ciò che da soli sarebbe impossibile. Quando
-          doni, ti unisci a una comunità globale di persone che condividono i
-          tuoi stessi valori. Questa unione crea una forza incredibile, capace
-          di affrontare sfide colossali. La tua donazione è parte di un
-          movimento più grande, un movimento che sta cambiando il mondo, un
-          passo alla volta. E insieme, possiamo fare ancora di più.
-        </p>
+          <div className="bg-white p-6 rounded-lg border-2 border-[#526742] shadow-md">
+            <h2 className="text-xl font-medium text-center mb-4 text-[#92aa7f]">
+              L'Unione fa la Forza
+            </h2>
+            <p className="text-justify text-sm">
+              Insieme, possiamo fare ciò che da soli sarebbe impossibile. Quando
+              doni, ti unisci a una comunità globale di persone che condividono
+              i tuoi stessi valori. Questa unione crea una forza incredibile,
+              capace di affrontare sfide colossali. La tua donazione è parte di
+              un movimento più grande, un movimento che sta cambiando il mondo,
+              un passo alla volta. E insieme, possiamo fare ancora di più.
+            </p>
+          </div>
 
-        <h2 className="text-xl font-medium text-center mt-6 mb-2 text-[#92aa7f]">
-          Lasciti un'Impronta Indelebile
-        </h2>
-        <p className="text-center text-sm">
-          Ogni donazione è un'impronta lasciata sulla storia. È una
-          testimonianza del fatto che hai scelto di fare la differenza, di agire
-          con compassione e determinazione. È un segno che, anche di fronte alle
-          avversità, hai deciso di rispondere con amore e generosità. Questo
-          segno non sbiadirà, rimarrà come un simbolo della tua volontà di
-          costruire un mondo migliore.
-        </p>
+          <div className="bg-white p-6 rounded-lg border-2 border-[#526742] shadow-md">
+            <h2 className="text-xl font-medium text-center mb-4 text-[#92aa7f]">
+              Lasci un'Impronta Indelebile
+            </h2>
+            <p className="text-justify text-sm">
+              Ogni donazione è un'impronta lasciata sulla storia. È una
+              testimonianza del fatto che hai scelto di fare la differenza, di
+              agire con compassione e determinazione. È un segno che, anche di
+              fronte alle avversità, hai deciso di rispondere con amore e
+              generosità. Questo segno non sbiadirà, rimarrà come un simbolo
+              della tua volontà di costruire un mondo migliore.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,137 +1,186 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-function Navbar({ bgColor, textColor, elementText, elementBg }) {
+function Navbar({
+  bgColor = "bg-green-600",
+  textColor = "text-white",
+  elementText = "text-white",
+  elementBg = "bg-gray-200",
+}) {
+  const [isOpen, setIsOpen] = useState(false);
   const [dropdownOpen, setDropdownOpen] = useState(false);
-  
-  const toggleDropdown = () => {
-    setDropdownOpen(!dropdownOpen);
-  };
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
 
   return (
     <nav className={`${bgColor} ${textColor} p-4`}>
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="flex items-center space-x-4">
-          <Link to="/" className="text-2xl font-semibold">
-            {/* si deve mettere qui il logo */}
-            Logo
-          </Link>
-
-          <ul className="hidden md:flex space-x-4">
-            <li>
-              <Link
-                to="/"
-                className={`${elementText} hover:text-pink transition duration-300`}
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/about"
-                className={`${elementText} hover:text-pink transition duration-300`}
-              >
-                Chi Siamo
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/community"
-                className={`${elementText} hover:text-pink transition duration-300`}
-              >
-                Community
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/adozioni"
-                className={`${elementText} hover:text-pink transition duration-300`}
-              >
-                Adozioni
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/donate"
-                className={`${elementText} hover:text-pink transition duration-300`}
-              >
-                Donazioni
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/shop"
-                className={`${elementText} hover:text-pink transition duration-300`}
-              >
-                Shop
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/blog"
-                className={`${elementText} hover:text-pink transition duration-300`}
-              >
-                Blog
-              </Link>
-            </li>
-          </ul>
+      <div className="flex items-center space-x-4">
+        {" "}
+        <Link to="/" className="text-2xl font-semibold">
+          {" "}
+          {/* si deve mettere qui il logo */} Logo{" "}
+        </Link>
+        <div className="hidden lg:flex items-center space-x-4">
+          <NavLink to="/" text="Home" />
+          <NavLink to="/about" text="About" />
+          <NavLink to="/community" text="Community" />
+          <NavLink to="/adozioni" text="Adozioni" />
+          <NavLink to="/donate" text="Donazioni" />
+          <NavLink to="/shop" text="Shop" />
+          <NavLink to="/blog" text="Blog" />
+          <DropdownMenu />
+          <AuthButtons />
         </div>
+     
+        <button
+          onClick={toggleMenu}
+          className="lg:hidden text-white focus:outline-none"
+        >
+          {isOpen ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          )}
+        </button>
+      </div>
 
-        <div className="flex items-center space-x-4">
-          <div className="relative">
-            <button
-              onClick={toggleDropdown}
-              className={`${elementText} hover:text-pink transition duration-300`}
-            >
-              Contatti
-            </button>
-            <ul
-              className={`absolute top-full right-0 mt-2 bg-white shadow-lg rounded-md p-2 ${
-                dropdownOpen ? "block" : "hidden"
-              }`}
-            >
-              <li>
-                <a
-                  href="https://www.facebook.com/"
-                  className="block px-4 py-2 hover:bg-gray-100"
-                >
-                  Facebook
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.instagram.com/"
-                  className="block px-4 py-2 hover:bg-gray-100"
-                >
-                  Instagram
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://twitter.com/home"
-                  className="block px-4 py-2 hover:bg-gray-100"
-                >
-                  Twitter
-                </a>
-              </li>
-            </ul>
-          </div>
-          <Link
-            to="/login"
-            className="bg-black text-white px-4 py-2 rounded hover:bg-gray-800 transition duration-300"
-          >
-            Accedi
-          </Link>
-          <Link
-            to="/register"
-            className={`${elementBg} bg-pink text-dark-grey px-4 py-2 rounded  hover:bg-opacity-90 transition duration-300`}
-          >
-            Registrati
-          </Link>
-        </div>
+     
+      <div className={`lg:hidden ${isOpen ? "block" : "hidden"} mt-4`}>
+        <NavLink to="/" text="Home" mobile />
+        <NavLink to="/about" text="About" mobile />
+        <NavLink to="/community" text="Community" mobile />
+        <NavLink to="/adozioni" text="Adozioni" mobile />
+        <NavLink to="/donate" text="Donazioni" mobile />
+        <NavLink to="/shop" text="Shop" mobile />
+        <NavLink to="/blog" text="Blog" mobile />
+        <DropdownMenu mobile />
+        <AuthButtons mobile />
       </div>
     </nav>
   );
+
+  function NavLink({ to, text, mobile }) {
+    return (
+      <Link
+        to={to}
+        className={`${
+          mobile ? "block py-2 px-4" : ""
+        } ${elementText} hover:text-pink-300 transition duration-300`}
+        onClick={() => mobile && setIsOpen(false)}
+      >
+        {text}
+      </Link>
+    );
+  }
+
+  function DropdownMenu({ mobile }) {
+    return (
+      <div className={mobile ? "" : "relative"}>
+        <button
+          onClick={toggleDropdown}
+          className={`${elementText} hover:text-pink-300 transition duration-300 ${
+            mobile ? "block w-full text-left py-2 px-4" : ""
+          }`}
+        >
+          Contatti
+        </button>
+        <ul
+          className={`${
+            mobile
+              ? "pl-4"
+              : "absolute top-full right-0 mt-2 bg-white shadow-lg rounded-md"
+          } ${dropdownOpen ? "block" : "hidden"}`}
+        >
+          <li>
+            <a
+              href="https://www.facebook.com/"
+              className={`block px-4 py-2 hover:bg-gray-100 ${
+                mobile ? "text-white" : "text-green-600"
+              }`}
+            >
+              Facebook
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/"
+              className={`block px-4 py-2 hover:bg-gray-100 ${
+                mobile ? "text-white" : "text-green-600"
+              }`}
+            >
+              Instagram
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://twitter.com/home"
+              className={`block px-4 py-2 hover:bg-gray-100 ${
+                mobile ? "text-white" : "text-green-600"
+              }`}
+            >
+              Twitter
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+
+  function AuthButtons({ mobile }) {
+    return (
+      <>
+        <Link
+          to="/login"
+          className={`${
+            mobile ? "block w-full" : ""
+          } bg-gray-800 text-white px-4 py-2 rounded hover:bg-gray-700 transition duration-300 ${
+            mobile ? "mt-2" : ""
+          }`}
+          onClick={() => mobile && setIsOpen(false)}
+        >
+          Accedi
+        </Link>
+        <Link
+          to="/register"
+          className={`${
+            mobile ? "block w-full" : ""
+          } ${elementBg} text-green-600 px-4 py-2 rounded hover:bg-opacity-90 transition duration-300 ${
+            mobile ? "mt-2" : ""
+          }`}
+          onClick={() => mobile && setIsOpen(false)}
+        >
+          Registrati
+        </Link>
+      </>
+    );
+  }
 }
 
 export default Navbar;
