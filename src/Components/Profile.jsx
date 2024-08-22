269bbeeb4e7d8a44de99f3b89ee19186a67aeced
auroraloreti <loretiaurora16@gmail.com>
2024-08-22 16:27:07 +0200

    modifiche profile

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export default function Profile() {
-  const [nickname, setNickName] = useState("user");
+  const [nickname, setNickName] = useState("USER");
   const [firstname, setfirstname] = useState("user's name");
   const [surname, setSurname] = useState("user's surname");
   const [bio, setBio] = useState("user's bio");
@@ -11,15 +11,15 @@ export default function Profile() {
 
   return (
     <>
-      <div className="profile-container">
-        <div className="flex flex-col items-center justify-center profile-card">
-          <h1>@{nickname}</h1>
-          <img src={icon} alt="" />
-          <p>
+      <div className="profile-container bg-light-grey">
+        <div className="flex flex-col items-center justify-center profile-card bg-white mt-6">
+          <h1 className=" font-semibold text-2xl text-dark-grey mb-3 "> @{nickname} </h1>
+          <img className="w-auto rounded-md object-cover   hover:" src={icon} alt="" />
+          <p className=" text-gray-700 mt-4 ">
             {firstname} {surname}'s Profile{" "}
           </p>
-          <h4>Bio</h4>
-          <p>{bio}</p>
+          <h4 className=" font-semibold text-2xl text-dark-grey "> Bio </h4>
+          <p className=" text-gray-700 "> {bio} </p>
         </div>
       </div>
     </>
